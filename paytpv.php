0e2654bab621c31bcd8c410657d586938f90e5ea
Carlos Babiano <cbabiano@paycomet.com>
2022-12-16 12:42:43 +0100

    Se añade verificación del prefijo teléfono

--- paytpv.php
+++ paytpv.php
@@ -47,7 +47,7 @@ class Paytpv extends PaymentModule
         $this->name = 'paytpv';
         $this->tab = 'payments_gateways';
         $this->author = 'Paycomet';
-        $this->version = '6.7.18';
+        $this->version = '6.7.19';
         $this->module_key = 'deef285812f52026197223a4c07221c4';
 
         $this->bootstrap = true;
@@ -780,12 +780,15 @@ class Paytpv extends PaymentModule
 
         // Se calculan los impuestos
         $tax = number_format($cart->getOrderTotal(true, Cart::BOTH) * 100, 0, '.', '') - $amount;
-        $i++;
-        $shoppingCartData[$key + $i]["sku"] = "1";
-        $shoppingCartData[$key + $i]["quantity"] = 1;
-        $shoppingCartData[$key + $i]["unitPrice"] = $tax;
-        $shoppingCartData[$key + $i]["name"] = "Tax";
-        $shoppingCartData[$key + $i]["articleType"] = "11";
+        
+        if ((int)$tax!=0) {
+            $i++;
+            $shoppingCartData[$key + $i]["sku"] = "1";
+            $shoppingCartData[$key + $i]["quantity"] = 1;
+            $shoppingCartData[$key + $i]["unitPrice"] = $tax;
+            $shoppingCartData[$key + $i]["name"] = "Tax";
+            $shoppingCartData[$key + $i]["articleType"] = "11";
+        }
 
         return array("shoppingCart" => array_values($shoppingCartData));
     }
@@ -840,6 +843,15 @@ class Paytpv extends PaymentModule
         return $isoCodeNumber;
     }
 
+    public function getPhonePrefix($phone)
+    {
+        $prefix_array = ['44', '213', '376', '244', '1264', '1268', '54', '374', '297', '61', '43', '994', '1242', '973', '880', '1246', '375', '32', '501', '229', '1441', '975', '591', '387', '267', '55', '673', '359', '226', '257', '855', '237', '1', '238', '1345', '236', '56', '86', '57', '269', '242', '682', '506', '385', '53', '90392', '357', '42', '45', '253', '1809', '1809', '593', '20', '503', '240', '291', '372', '251', '500', '298', '679', '358', '33', '594', '689', '241', '220', '7880', '49', '233', '350', '30', '299', '1473', '590', '671', '502', '224', '245', '592', '509', '504', '852', '36', '354', '91', '62', '98', '964', '353', '972', '39', '1876', '81', '962', '7', '254', '686', '850', '82', '965', '996', '856', '371', '961', '266', '231', '218', '417', '370', '352', '853', '389', '261', '265', '60', '960', '223', '356', '692', '596', '222', '269', '52', '691', '373', '377', '976', '1664', '212', '258', '95', '264', '674', '977', '31', '687', '64', '505', '227', '234', '683', '672', '670', '47', '968', '680', '507', '675', '595', '51', '63', '48', '351', '1787', '974', '262', '40', '7', '250', '378', '239', '966', '221', '381', '248', '232', '65', '421', '386', '677', '252', '27', '34', '94', '290', '1869', '1758', '249', '597', '268', '46', '41', '963', '886', '7', '66', '228', '676', '1868', '216', '90', '7', '993', '1649', '688', '256', '380', '971', '598', '7', '678', '379', '58', '84', '84', '84', '681', '969', '967', '260', '1', '263'];
+        foreach ($prefix_array as $key => $prefix) {
+            if (str_starts_with($phone, '+' . $prefix))
+                return $prefix;
+        }
+    }
+
     public function getEMV3DS($cart)
     {
         $Merchant_EMV3DS = array();
@@ -888,10 +900,12 @@ class Paytpv extends PaymentModule
             if ($billing->phone) {
                 $arrDatosHomePhone = array();
 
+                $phone_prefix = str_starts_with($billing->phone, '+') ? $this->getPhonePrefix($billing->phone) : $billing_address_country->call_prefix;
+
                 $arrDatosHomePhone["cc"] =
-                    Tools::substr(preg_replace('/[^0-9]/', '', $billing_address_country->call_prefix), 0, 3);
+                    Tools::substr(preg_replace('/[^0-9]/', '', $phone_prefix), 0, 3);
                 $arrDatosHomePhone["subscriber"] =
-                    Tools::substr(preg_replace('/[^0-9]/', '', $billing->phone), 0, 15);
+                    Tools::substr(preg_replace('/[^0-9]/', '', str_replace("+" . $phone_prefix, "", $billing->phone)), 0, 15);
 
                 $Merchant_EMV3DS["customer"]["homePhone"] = $arrDatosHomePhone;
                 $Merchant_EMV3DS["customer"]["mobilePhone"] = $arrDatosHomePhone;
@@ -900,10 +914,12 @@ class Paytpv extends PaymentModule
             if ($billing->phone_mobile) {
                 $arrDatosMobilePhone = array();
 
+                $phone_prefix = str_starts_with($billing->phone_mobile, '+') ? $this->getPhonePrefix($billing->phone_mobile) : $billing_address_country->call_prefix;
+
                 $arrDatosMobilePhone["cc"] =
-                    Tools::substr(preg_replace('/[^0-9]/', '', $billing_address_country->call_prefix), 0, 3);
+                    Tools::substr(preg_replace('/[^0-9]/', '', $phone_prefix), 0, 3);
                 $arrDatosMobilePhone["subscriber"] =
-                    Tools::substr(preg_replace('/[^0-9]/', '', $billing->phone_mobile), 0, 15);
+                    Tools::substr(preg_replace('/[^0-9]/', '', str_replace("+" . $phone_prefix, "", $billing->phone_mobile)), 0, 15);
 
                 $Merchant_EMV3DS["customer"]["mobilePhone"] = $arrDatosMobilePhone;
             }
@@ -939,10 +955,12 @@ class Paytpv extends PaymentModule
             if ($shipping->phone) {
                 $arrDatosWorkPhone = array();
 
+                $phone_prefix = str_starts_with($shipping->phone, '+') ? $this->getPhonePrefix($shipping->phone) : $shipping_address_country->call_prefix;
+
                 $arrDatosWorkPhone["cc"] =
-                    Tools::substr(preg_replace('/[^0-9]/', '', $shipping_address_country->call_prefix), 0, 3);
+                    Tools::substr(preg_replace('/[^0-9]/', '', $phone_prefix), 0, 3);
                 $arrDatosWorkPhone["subscriber"] =
-                    Tools::substr(preg_replace('/[^0-9]/', '', $shipping->phone), 0, 15);
+                    Tools::substr(preg_replace('/[^0-9]/', '', str_replace("+" . $phone_prefix, "", $shipping->phone)), 0, 15);
 
                 $Merchant_EMV3DS["customer"]["workPhone"] = $arrDatosWorkPhone;
             }
